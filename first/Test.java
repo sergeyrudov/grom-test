3626edd189c0ba89e39cd9a81a9944cf70eba30f
rudovs <extrazeto@gmail.com>
2019-03-16 01:31:27 +0200

    init commit

---
+++ first/Test.java
@@ -0,0 +1,12 @@
+package first;
+
+public class Test {
+    public static void main(String[] args) {
+        String a = "abc";
+        sout(a);
+    }
+
+    static void sout(String a){
+        System.out.println(a);
+    }
+}
---
+++ third/Test.java
@@ -0,0 +1,13 @@
+package third;
+
+public class Test {
+    public static void main(String[] args) {
+        String a = "abc";
+        sout(a);
+    }
+
+    static void sout(String a){
+        System.out.println(a);
+    }
+}
+
